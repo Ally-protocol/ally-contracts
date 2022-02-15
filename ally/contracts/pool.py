cbd023890e7e5d098768c9ea7cf4663073daaa53
Che Jin <robin912104@gmail.com>
2022-02-15 10:24:05 -0600

    Refactor pool.py with demo code

--- ally/contracts/pool.py
+++ ally/contracts/pool.py
@@ -1,45 +1,186 @@
 import os
 
 from pyteal import *
+from pytealutils.storage import global_get_else
+from pytealutils.strings import itoa
 
-total_supply = 0xFFFFFFFFFFFFFFFF
+total_supply = 0xFFFFFFFFFFFFFFFF;
+seed_amount = 0xFFFFFFFFFFFFFFFFFFF;
 
+gov_key = Bytes("gov")
+pool_token_key = Bytes("p")
 
-class AllyPool:
-    class Vars:
-        gov_key = Bytes("gov")
-        pool_token_key = Bytes("p")
-        mint_price_key = Bytes("mp")
-        redeem_price_key = Bytes("rp")
-        commited_algos_key = Bytes("co")
-        allow_redeem_key = Bytes("ar")
+action_update = Bytes("update")
+action_boot = Bytes("boot")
+action_join = Bytes("join")
+action_vote = Bytes("vote")
+action_exit = Bytes("exit")
 
-    @staticmethod
+# Takes unix timestamp for locked windows
+def approval(lock_start: int = 0, lock_stop: int = 0):
+    assert lock_start < lock_stop
+
+    # Alias commonly used things
+    me = Global.current_application_address()
+    pool_token = App.globalGet(pool_token_key)
+    pool_balance = AssetHolding.balance(me, pool_token)
+    governor = global_get_else(gov_key, Global.creator_address())
+
+    # Checks for if we're in the window for this action
+    before_lock_start = Global.latest_timestamp() < Int(lock_start)
+    after_lock_stop = Global.latest_timestamp() > Int(lock_stop)
+
     @Subroutine(TealType.uint64)
-    def mint_tokens(algos_in: TealType.uint64):
+    def mint_tokens(algos_in):
         # Mint in 1:1 with algos passed in
-        mint_amount = WideRatio(
-            [App.globalGet(AllyPool.Vars.mint_price_key), algos_in],
-            [Int(1_000_000_000)]
-        )
-        return Seq(
-            Return(mint_amount)
-        )
+        return algos_in
 
-    @staticmethod
     @Subroutine(TealType.uint64)
-    def algos_to_redeem(amt: TealType.uint64):
-        algos = WideRatio(
-            [App.globalGet(AllyPool.Vars.redeem_price_key), amt],
-            [Int(1_000_000_000)]
-        )
-        return Seq(
-            Return(algos)
+    def burn_tokens(
+        amt,
+        balance_algos,
+        tokens_minted,
+    ):
+        # Return the number of tokens * (algos per token)
+        return amt * ((balance_algos - Int(seed_amount)) / tokens_minted)
+
+    @Subroutine(TealType.uint64)
+    def join():
+        # Alias for specific txn indicies
+        app_call, payment = Gtxn[0], Gtxn[1]
+        well_formed_join = And(
+            Global.group_size() == Int(2),  # App call, Payment to join
+            app_call.type_enum() == TxnType.ApplicationCall,
+            app_call.assets[0] == pool_token,
+            payment.type_enum() == TxnType.Payment,
+            payment.receiver() == me,
+            payment.amount() > Int(0),
+            payment.sender() == app_call.sender(),
         )
 
-    @staticmethod
+        return Seq(
+            # Init MaybeValues
+            pool_balance,
+            # TODO: uncomment when done testing on dev
+            # Assert(before_lock_start),
+            Assert(well_formed_join),
+            axfer(app_call.sender(), pool_token, mint_tokens(payment.amount())),
+            Int(1),
+        )
+
+    # Action to allow caller to exit the contract, burning pool tokens in exchange for algos
+    @Subroutine(TealType.uint64)
+    def exit():
+        # Alias for specific txn indicies
+        app_call, pool_xfer = Gtxn[0], Gtxn[1]
+        well_formed_exit = And(
+            Global.group_size() == Int(2),
+            app_call.type_enum() == TxnType.ApplicationCall,
+            app_call.assets[0] == pool_token,
+            pool_xfer.type_enum() == TxnType.AssetTransfer,
+            pool_xfer.asset_receiver() == me,
+            pool_xfer.xfer_asset() == pool_token,
+            app_call.sender() == pool_xfer.sender(),
+        )
+
+        return Seq(
+            pool_balance,
+            # TODO: uncomment when done testing on dev
+            # Assert(after_lock_stop),
+            Assert(well_formed_exit),
+            # Looks good, pay 'em
+            pay(
+                pool_xfer.sender(),
+                burn_tokens(pool_xfer.asset_amount(), Balance(me), get_minted()),
+            ),
+            Int(1),
+        )
+
+    # Action to allow admin to commit algos or vote
+    @Subroutine(TealType.uint64)
+    def vote():
+        app_call = Gtxn[0]
+        well_formed_vote = And(
+            Global.group_size() == Int(1),
+            app_call.type_enum() == TxnType.ApplicationCall,
+            app_call.sender() == governor,
+        )
+        return Seq(
+            # TODO: assert we're in the voting window
+            Assert(well_formed_vote),
+            InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields(
+                {
+                    TxnField.type_enum: TxnType.Payment,
+                    TxnField.receiver: Txn.accounts[1],
+                    TxnField.amount: Int(0),  # not strictly necessary
+                    TxnField.note: Txn.application_args[
+                        1
+                    ],  # expecting a valid note as the 2nd element in app args array
+                    TxnField.fee: Int(0),
+                }
+            ),
+            InnerTxnBuilder.Submit(),
+            Int(1),
+        )
+
+    # Action to Bootstrap this application, creates pool token
+    @Subroutine(TealType.uint64)
+    def bootstrap():
+        seed, app_call = Gtxn[0], Gtxn[1]
+        well_formed_bootstrap = And(
+            Global.group_size() == Int(2),
+            # Seed amount so it can send transactions
+            seed.type_enum() == TxnType.Payment,
+            seed.amount() == Int(seed_amount),
+            app_call.type_enum() == TxnType.ApplicationCall,
+            app_call.sender() == governor,
+            app_call.sender() == seed.sender(),
+        )
+
+        pool_token_check = App.globalGetEx(Int(0), pool_token_key)
+
+        return Seq(
+            pool_token_check,
+            # Make sure we've not already set this
+            Assert(Not(pool_token_check.hasValue())),
+            Assert(well_formed_bootstrap),
+            # Create the pool token
+            InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields(
+                {
+                    TxnField.type_enum: TxnType.AssetConfig,
+                    TxnField.config_asset_name: Concat(
+                        Bytes("GovernanceToken-"), itoa(Global.current_application_id())
+                    ),
+                    TxnField.config_asset_unit_name: Bytes("algo-gov"),
+                    TxnField.config_asset_total: Int(total_supply),
+                    TxnField.config_asset_manager: me,
+                    TxnField.config_asset_reserve: me,
+                    TxnField.fee: Int(0),
+                }
+            ),
+            InnerTxnBuilder.Submit(),
+            # Write it to global state
+            App.globalPut(pool_token_key, InnerTxn.created_asset_id()),
+            Int(1),
+        )
+
+    # Return the number of tokens minted
+    @Subroutine(TealType.uint64)
+    def get_minted():
+        return Seq(pool_balance, Int(total_supply) - pool_balance.value())
+
+    # Overwrite the current governor
+    @Subroutine(TealType.uint64)
+    def set_governor(new_gov):
+        return Seq(
+            Assert(Txn.sender() == governor), App.globalPut(gov_key, new_gov), Int(1)
+        )
+
+    # Util function to transfer an asset
     @Subroutine(TealType.none)
-    def axfer(receiver: TealType.bytes, aid: TealType.uint64, amt: TealType.uint64):
+    def axfer(reciever, aid, amt):
         return Seq(
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields(
@@ -47,229 +188,64 @@ class AllyPool:
                     TxnField.type_enum: TxnType.AssetTransfer,
                     TxnField.xfer_asset: aid,
                     TxnField.asset_amount: amt,
-                    TxnField.asset_receiver: receiver,
+                    TxnField.asset_receiver: reciever,
+                    TxnField.fee: Int(0),  # force caller to pay
                 }
             ),
             InnerTxnBuilder.Submit(),
         )
 
-    @staticmethod
+    # Util function to make payment transaction
     @Subroutine(TealType.none)
-    def pay(receiver: TealType.bytes, amt: TealType.uint64):
+    def pay(receiver, amt):
         return Seq(
             InnerTxnBuilder.Begin(),
             InnerTxnBuilder.SetFields(
                 {
                     TxnField.type_enum: TxnType.Payment,
                     TxnField.amount: amt,
-                    TxnField.receiver: receiver
+                    TxnField.receiver: receiver,
+                    TxnField.fee: Int(0),  # force caller to pay
                 }
             ),
             InnerTxnBuilder.Submit(),
         )
 
-    def on_create(self):
-        return Seq(
-            App.globalPut(self.Vars.mint_price_key, Int(1_000_000_000)),
-            App.globalPut(self.Vars.redeem_price_key, Int(1_000_000_000)),
-            App.globalPut(self.Vars.allow_redeem_key, Int(1)),
-            App.globalPut(self.Vars.commited_algos_key, Int(0)),
-            App.globalPut(self.Vars.gov_key, Txn.sender()),
-            Approve()
-        )
+    router = Cond(
+        [Txn.application_args[0] == action_join, join()],
+        [Txn.application_args[0] == action_exit, exit()],
+        [Txn.application_args[0] == action_boot, bootstrap()],
+        [Txn.application_args[0] == action_update, set_governor(Txn.accounts[1])],
+        [Txn.application_args[0] == action_vote, vote()],
+    )
 
-    def on_bootstrap(self):
-        pool_token_check = App.globalGetEx(Int(0), self.Vars.pool_token_key)
-        governor = App.globalGet(self.Vars.gov_key)
-        return Seq(
-            pool_token_check,
-            # Make sure we've not already set this
-            Assert(Not(pool_token_check.hasValue())),
-            Assert(Txn.sender() == governor),
-            # Create the pool token
-            InnerTxnBuilder.Begin(),
-            InnerTxnBuilder.SetFields({
-                TxnField.type_enum: TxnType.AssetConfig,
-                TxnField.config_asset_name: Bytes("wALGO"),
-                TxnField.config_asset_unit_name: Bytes("wALGO"),
-                TxnField.config_asset_url: Bytes("https://maxos.studio"),
-                TxnField.config_asset_total: Int(total_supply),
-                TxnField.config_asset_decimals: Int(6),
-                TxnField.config_asset_manager: Global.zero_address(),
-                TxnField.config_asset_reserve: Global.zero_address(),
-                TxnField.config_asset_clawback: Global.zero_address(),
-                TxnField.config_asset_freeze: Global.zero_address(),
-            }),
-            InnerTxnBuilder.Submit(),
-            # Write it to global state
-            App.globalPut(self.Vars.pool_token_key,
-                          InnerTxn.created_asset_id()),
-            Approve(),
-        )
+    return Cond(
+        [Txn.application_id() == Int(0), Int(1)],
+        [Txn.on_completion() == OnComplete.DeleteApplication, Txn.sender() == governor],
+        [Txn.on_completion() == OnComplete.UpdateApplication, Txn.sender() == governor],
+        [Txn.on_completion() == OnComplete.CloseOut, Int(1)],
+        [Txn.on_completion() == OnComplete.OptIn, Int(0)],
+        [Txn.on_completion() == OnComplete.NoOp, router],
+    )
 
-    def on_set_governor(self):
-        new_gov = Txn.accounts[1]
-        governor = App.globalGet(self.Vars.gov_key)
-        return Seq(
-            Assert(Txn.sender() == governor),
-            App.globalPut(self.Vars.gov_key, new_gov),
-            Approve()
-        )
 
-    def on_set_mint_price(self):
-        new_mint_price = Txn.application_args[1]
-        governor = App.globalGet(self.Vars.gov_key)
-        return Seq(
-            Assert(Txn.sender() == governor),
-            App.globalPut(self.Vars.mint_price_key, Btoi(new_mint_price)),
-            Approve()
-        )
+def clear():
+    return Return(Int(1))
 
-    def on_set_redeem_price(self):
-        new_redeem_price = Txn.application_args[0]
-        governor = App.globalGet(self.Vars.gov_key)
-        return Seq(
-            Assert(Txn.sender() == governor),
-            App.globalPut(self.Vars.redeem_price_key, Btoi(new_redeem_price)),
-            Approve()
-        )
 
-    def on_toggle_redeem(self):
-        governor = App.globalGet(self.Vars.gov_key)
-        return Seq(
-            Assert(Txn.sender() == governor),
-            App.globalPut(self.Vars.allow_redeem_key, Not(
-                App.globalGet(self.Vars.allow_redeem_key))),
-            Approve()
-        )
+def get_approval_src(**kwargs):
+    return compileTeal(approval(**kwargs), mode=Mode.Application, version=6)
 
-    def on_join(self):
-        governor = App.globalGet(self.Vars.gov_key)
-        return Seq(
-            Assert(
-                And(
-                    Txn.type_enum() == TxnType.ApplicationCall,
-                    Txn.sender() == governor,
-                )
-            ),
-            InnerTxnBuilder.Begin(),
-            InnerTxnBuilder.SetFields({
-                TxnField.type_enum: TxnType.Payment,
-                TxnField.receiver: Txn.accounts[1],  # address of goverance
-                TxnField.amount: Int(0),
-                TxnField.note: Txn.application_args[
-                    1
-                ],  # expecting a valid note as the 2nd element in app args array
-            }),
-            InnerTxnBuilder.Submit(),
-            Approve(),
-        )
 
-    def on_vote(self):
-        algos_to_commit = Btoi(Txn.application_args[1])
-        return Seq(
-            # TODO:
-            Approve(),
-        )
-
-    def on_mint(self):
-        pool_token = App.globalGet(self.Vars.pool_token_key)
-        pool_bal = AssetHolding.balance(
-            Global.current_application_address(), pool_token)
-        return Seq(
-            # Init MaybeValues
-            pool_bal,
-            Assert(
-                And(
-                    Global.group_size() == Int(2),  # App call, Payment to mint
-                    Gtxn[0].type_enum() == TxnType.ApplicationCall,
-                    Gtxn[0].assets[0] == pool_token,
-                    Gtxn[1].type_enum() == TxnType.Payment,
-                    Gtxn[1].receiver() == Global.current_application_address(),
-                    Gtxn[1].amount() > Int(1_000),
-                    Gtxn[1].sender() == Gtxn[0].sender(),
-                )
-            ),
-            self.axfer(
-                Gtxn[0].sender(),
-                pool_token,
-                self.mint_tokens(Gtxn[1].amount() - Int(1_000))
-            ),
-            Approve(),
-        )
-
-    def on_redeem(self):
-        pool_token = App.globalGet(self.Vars.pool_token_key)
-        pool_bal = AssetHolding.balance(
-            Global.current_application_address(), pool_token)
-        return Seq(
-            Assert(App.globalGet(self.Vars.allow_redeem_key)),
-            Assert(
-                And(
-                    Global.group_size() == Int(2),
-                    Gtxn[0].type_enum() == TxnType.ApplicationCall,
-                    Gtxn[0].assets[0] == pool_token,
-                    Gtxn[1].type_enum() == TxnType.AssetTransfer,
-                    Gtxn[1].asset_receiver() == Global.current_application_address(),
-                    Gtxn[1].xfer_asset() == pool_token,
-                    Gtxn[0].sender() == Gtxn[1].sender(),
-                )
-            ),
-            pool_bal,
-            self.pay(Gtxn[1].sender(), self.algos_to_redeem(Gtxn[1].asset_amount())),
-            Approve(),
-        )
-
-    def on_call(self):
-        on_call_method = Txn.application_args[0]
-        return Cond(
-            # Admin
-            [on_call_method == Bytes("bootstrap"), self.on_bootstrap()],
-            [on_call_method == Bytes("set_governor"), self.on_set_governor()],
-            [on_call_method == Bytes("set_mint_price"),
-             self.on_set_mint_price()],
-            [on_call_method == Bytes("set_redeem_price"),
-             self.on_set_redeem_price()],
-            [on_call_method == Bytes("toggle_redeem"),
-             self.on_toggle_redeem()],
-            [on_call_method == Bytes("join"), self.on_join()],
-            [on_call_method == Bytes("vote"), self.on_vote()],
-            # Users
-            [on_call_method == Bytes("mint"), self.on_mint()],
-            [on_call_method == Bytes("redeem"), self.on_redeem()],
-        )
-
-    def approval_program(self):
-        governor = App.globalGet(self.Vars.gov_key)
-        program = Cond(
-            [Txn.application_id() == Int(0), self.on_create()],
-            [
-                Txn.on_completion() == OnComplete.DeleteApplication,
-                Return(Txn.sender() == governor)
-            ],
-            [
-                Txn.on_completion() == OnComplete.UpdateApplication,
-                Return(Txn.sender() == governor)
-            ],
-            [Txn.on_completion() == OnComplete.CloseOut, Approve()],
-            [Txn.on_completion() == OnComplete.OptIn, Reject()],
-            [Txn.on_completion() == OnComplete.NoOp, self.on_call()],
-        )
-        return program
-
-    def clear_program(self):
-        return Approve()
+def get_clear_src(**kwargs):
+    return compileTeal(clear(**kwargs), mode=Mode.Application, version=6)
 
 
 if __name__ == "__main__":
     path = os.path.dirname(os.path.abspath(__file__))
-    pool = AllyPool()
-    with open(os.path.join(path, "approval.teal"), "w") as f:
-        compiled = compileTeal(pool.approval_program(),
-                               mode=Mode.Application, version=5)
-        f.write(compiled)
 
-    with open(os.path.join(path, "clear.teal"), "w") as f:
-        compiled = compileTeal(pool.clear_program(),
-                               mode=Mode.Application, version=5)
-        f.write(compiled)
+    with open(os.path.join(path, "pool_approval.teal"), "w") as f:
+        f.write(get_approval_src(lock_start=1, lock_stop=10))
+
+    with open(os.path.join(path, "pool_clear.teal"), "w") as f:
+        f.write(get_clear_src())
