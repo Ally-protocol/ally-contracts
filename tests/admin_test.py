cc90696ca3bab463c06aa86b70b8902bf3e12090
maxie <maxiperezc@gmail.com>
2022-02-18 22:05:00 -0300

    admin tests tweaks

--- tests/admin_test.py
+++ tests/admin_test.py
@@ -12,84 +12,54 @@ from ally.account import Account
 from ally.operations import set_mint_price, set_redeem_price, toggle_redeem, set_governor, set_multisig_governor
 from ally.utils import get_algod_client, get_app_global_state, get_governors
 
-ALLOWED_SHIFT = 2.5 # percent
 
-MIN = 1 - ALLOWED_SHIFT/100
-MAX = 1 + ALLOWED_SHIFT/100
+dotenv.load_dotenv('.env')
+client = get_algod_client(os.environ.get("ALGOD_URL"), os.environ.get("ALGOD_API_KEY"))
+app_id = int(os.environ.get("APP_ID"))
+version = 1
+threshold = int(os.environ.get("MULTISIG_THRESHOLD"))
+governors = get_governors()
 
 def test_mint_price():
-    dotenv.load_dotenv('.env')
-
-    client = get_algod_client(os.environ.get("ALGOD_URL"), os.environ.get("ALGOD_API_KEY"))
-    app_id = int(os.environ.get("APP_ID"))
-    version = 1
-    threshold = int(os.environ.get("MULTISIG_THRESHOLD"))
-    governors = get_governors()
-
-    new_mint_price = int(1_000_000_700)
-
     state = get_app_global_state(client, app_id)
     current_mint_price = state[b"mp"]
+    new_mint_price = current_mint_price + 1_000
 
-    shift = (new_mint_price/current_mint_price)
-    assert shift >= MIN and shift <= MAX
+    assert current_mint_price > 0
 
     set_mint_price(new_mint_price, client, governors, app_id, version, threshold)
 
     state = get_app_global_state(client, app_id)
-    actual_mint_price = state[b"mp"]
-    assert new_mint_price == actual_mint_price
+    current_mint_price = state[b"mp"]
+    assert new_mint_price == current_mint_price
 
 def test_redeem_price():
-    dotenv.load_dotenv('.env')
-
-    client = get_algod_client(os.environ.get("ALGOD_URL"), os.environ.get("ALGOD_API_KEY"))
-    app_id = int(os.environ.get("APP_ID"))
-    version = 1
-    threshold = int(os.environ.get("MULTISIG_THRESHOLD"))
-    governors = get_governors()
-
-    new_redeem_price = int(1_000_000_500)
-
     state = get_app_global_state(client, app_id)
     current_redeem_price = state[b"rp"]
+    new_redeem_price = current_redeem_price + 1_000
 
-    shift = (new_redeem_price/current_redeem_price)
-    assert shift >= MIN and shift <= MAX
+    assert current_redeem_price > 0
 
     set_redeem_price(new_redeem_price, client, governors, app_id, version, threshold)
 
     state = get_app_global_state(client, app_id)
-    actual_redeem_price = state[b"rp"]
-    assert new_redeem_price == actual_redeem_price
+    current_redeem_price = state[b"rp"]
+    assert new_redeem_price == current_redeem_price
 
 def test_toggle_redeem():
-    dotenv.load_dotenv('.env')
-
-    client = get_algod_client(os.environ.get("ALGOD_URL"), os.environ.get("ALGOD_API_KEY"))
-    app_id = int(os.environ.get("APP_ID"))
-    version = 1
-    threshold = int(os.environ.get("MULTISIG_THRESHOLD"))
-    governors = get_governors()
-
     state = get_app_global_state(client, app_id)
-    current_redeem_key_status = state[b"ar"]
+    redeem_state = state[b"ar"]
 
     toggle_redeem(client, governors, app_id, version, threshold)
 
     state = get_app_global_state(client, app_id)
-    new_redeem_key_status = state[b"ar"]
+    new_redeem_state = state[b"ar"]
 
-    assert current_redeem_key_status != new_redeem_key_status
+    assert redeem_state == 1 - new_redeem_state
 
 def test_set_governor_as_funder():
-    client = get_algod_client(os.environ.get("ALGOD_URL"), os.environ.get("ALGOD_API_KEY"))
-    app_id = int(os.environ.get("APP_ID"))
-    version = 1
-    governors = get_governors()
-
     creator = Account.from_mnemonic(os.environ.get("FUNDER_MNEMONIC"))
-    threshold = 3
+    threshold = int(os.environ.get("MULTISIG_THRESHOLD"))
     
     set_governor(client, creator, app_id, governors, version, threshold)
 
@@ -99,12 +69,6 @@ def test_set_governor_as_funder():
     assert creator.get_address() == encoding.encode_address(new_governor)
 
 def test_set_governor_as_multisig():
-    client = get_algod_client(os.environ.get("ALGOD_URL"), os.environ.get("ALGOD_API_KEY"))
-    app_id = int(os.environ.get("APP_ID"))
-    version = 1
-    threshold = int(os.environ.get("MULTISIG_THRESHOLD"))
-    governors = get_governors()
-
     creator = Account.from_mnemonic(os.environ.get("FUNDER_MNEMONIC"))
     msig = transaction.Multisig(
         version, threshold, 
